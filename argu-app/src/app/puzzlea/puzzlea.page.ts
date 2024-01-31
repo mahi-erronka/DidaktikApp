cefadfcde9a4ef23378876bd70f68bef09b3a325
Markel Rodriguez <96288766+MarkelRodriguez@users.noreply.github.com>
2024-01-31 13:17:36 +0100

    puzzlea bukatuta

--- argu-app/src/app/puzzlea/puzzlea.page.ts
+++ argu-app/src/app/puzzlea/puzzlea.page.ts
@@ -1,91 +1,191 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef, Renderer2, HostListener } from '@angular/core';
+import { StatusBar } from '@capacitor/status-bar';
 
 
 interface PuzzlePiece {
-  id: number;
-  url: string;
+  placed: boolean;
+  src: string;
+  x: number;
+  y: number;
+  width: number;
+  height: number;
+  zIndex: number;
 }
+
 @Component({
   selector: 'app-puzzlea',
   templateUrl: './puzzlea.page.html',
   styleUrls: ['./puzzlea.page.scss'],
 })
+
+
 export class PuzzleaPage implements OnInit {
+  
+  containerWidth = 400;
+  containerHeight = 300;
 
+  rows = 3;
+  cols = 4;
 
-  puzzle: PuzzlePiece[][] = [];
-  emptyPosition!: { row: number, col: number };
+  pieceWidthPercentage = 50/this.cols;
+  pieceHeightPercentage = 50/this.rows;
 
+  
+  originalPositions: PuzzlePiece[]=[
+    {src: '../../assets/img/pieza1.jpg', x: 225, y: 433.35, width: 75, height: 66.67, zIndex: 1, placed: true},
+    {src: '../../assets/img/pieza2.jpg', x: 150, y: 433.35, width: 75, height: 66.67, zIndex: 2, placed: true},
+    {src: '../../assets/img/pieza3.jpg', x: 75, y: 433.35, width: 75, height: 66.67, zIndex: 3, placed: true},
+    {src: '../../assets/img/pieza4.jpg', x: 0, y: 433.35, width: 75, height: 66.67, zIndex: 4, placed: true},
+    {src: '../../assets/img/pieza5.jpg', x: 225, y: 366.68, width: 75, height: 66.67, zIndex: 5, placed: true},
+    {src: '../../assets/img/pieza6.jpg', x: 150, y: 366.68, width: 75, height: 66.67, zIndex: 6, placed: true},
+    {src: '../../assets/img/pieza7.jpg', x: 75, y: 366.68, width: 75, height: 66.67, zIndex: 7, placed: true},
+    {src: '../../assets/img/pieza8.jpg', x: 0, y: 366.68, width: 75, height: 66.67, zIndex: 8, placed: true},
+    {src: '../../assets/img/pieza9.jpg', x: 225, y: 300.01, width: 75, height: 66.67, zIndex: 9, placed: true},
+    {src: '../../assets/img/pieza10.jpg', x: 150, y: 300.01, width: 75, height: 66.67, zIndex: 10, placed: true},
+    {src: '../../assets/img/pieza11.jpg', x: 75, y: 300.01, width: 75, height: 66.67, zIndex: 11, placed: true},
+    {src: '../../assets/img/pieza12.jpg', x: 0, y: 300.01, width: 75, height: 66.67, zIndex: 12, placed: true}
+  ];
+
+ 
+  
+  pieces: PuzzlePiece[]=[
+  {src: '../../assets/img/pieza1.jpg', x: 0, y: 0, width: 75, height: 66.67, zIndex: 1, placed: false},
+  {src: '../../assets/img/pieza2.jpg', x: 0, y: 0, width: 75, height: 66.67, zIndex: 2, placed: false},
+  {src: '../../assets/img/pieza3.jpg', x: 0, y: 0, width: 75, height: 66.67, zIndex: 3, placed: false},
+  {src: '../../assets/img/pieza4.jpg', x: 0, y: 0, width: 75, height: 66.67, zIndex: 4, placed: false},
+  {src: '../../assets/img/pieza5.jpg', x: 0, y: 0, width: 75, height: 66.67, zIndex: 5, placed: false},
+  {src: '../../assets/img/pieza6.jpg', x: 0, y: 0, width: 75, height: 66.67, zIndex: 6, placed: false},
+  {src: '../../assets/img/pieza7.jpg', x: 0, y: 0, width: 75, height: 66.67, zIndex: 7, placed: false},
+  {src: '../../assets/img/pieza8.jpg', x: 0, y: 0, width: 75, height: 66.67, zIndex: 8, placed: false},
+  {src: '../../assets/img/pieza9.jpg', x: 0, y: 0, width: 75, height: 66.67, zIndex: 9, placed: false},
+  {src: '../../assets/img/pieza10.jpg', x: 0, y: 0, width: 75, height: 66.67, zIndex: 10, placed: false},
+  {src: '../../assets/img/pieza11.jpg', x: 0, y: 0, width: 75, height: 66.67, zIndex: 11, placed: false},
+  {src: '../../assets/img/pieza12.jpg', x: 0, y: 0, width: 75, height: 66.67, zIndex: 12, placed: false}
+  ];
+  
+  selectedPieceIndex: number = -1;
+  offsetX: number = 0;
+  offsetY: number = 0;
+  completed: boolean = false;
   constructor() { 
-    this.initializePuzzle();
-  }
-
-  initializePuzzle() {
-    const imageUrls = [
-      '../../assets/img/pieza1.jpg','../../assets/img/pieza2.jpg','../../assets/img/pieza3.jpg','../../assets/img/pieza4.jpg','../../assets/img/pieza5.jpg','../../assets/img/pieza6.jpg','../../assets/img/pieza7.jpg','../../assets/img/pieza8.jpg','../../assets/img/pieza9.jpg','../../assets/img/pieza10.jpg','../../assets/img/pieza11.jpg','../../assets/img/pieza12.jpg'
-    ];
-    //piezak nahastu 
-    imageUrls.sort(() => Math.random() - 0.5);
-
-    //piezak sortu
-    const pieces: PuzzlePiece[] = imageUrls.map((url, index) => ({ id: index + 1, url }));
-
-    //piezak filetan eta zutabeetan zatitu
-    const rows = 3; 
-    const cols = 4; 
     
-    this.puzzle = [];
-    for (let i = 0; i < rows; i++) {
-      this.puzzle.push(pieces.slice(i * cols, i * cols + cols));
-    }
-
-    this.emptyPosition = this.findEmptyPosition();
-
   }
-  findEmptyPosition(): { row: number, col: number } {
-    for (let row = 0; row < this.puzzle.length; row++) {
-      for (let col = 0; col < this.puzzle[row].length; col++) {
-        if (this.puzzle[row][col].id === 0) {
-          return { row, col };
+  
+  /*ngAfterViewInit() {
+    const element = this.el.nativeElement;
+
+    // Escucha eventos táctiles
+    this.renderer.listen(element, 'touchstart', (event: TouchEvent) => this.startDrag(event));
+  }*/
+  selectPiece(index: number) {
+    this.selectedPieceIndex = index;
+    this.offsetX = 0;
+    this.offsetY = 0;
+  }
+
+  startDrag(event: TouchEvent) {
+    event.preventDefault();
+    if ('touches' in event) {
+
+      const touch = event.touches[0];
+      const touchedElement = document.elementFromPoint(touch.clientX, touch.clientY);
+      
+
+      if (touchedElement && touchedElement.classList.contains('piece')){
+        
+        
+        
+        if (this.selectedPieceIndex !== -1) {
+          this.offsetX = touch.clientX - this.pieces[this.selectedPieceIndex].x;
+          this.offsetY = touch.clientY - this.pieces[this.selectedPieceIndex].y;
+
+          document.addEventListener('touchmove', this.handleDrag.bind(this));
+          document.addEventListener('touchend', this.endDrag.bind(this));
         }
       }
     }
-    return { row: -1, col: -1 }; // No debería ocurrir si la lógica es correcta
+    /*if (this.selectedPieceIndex !== -1) {
+      this.offsetX = event.clientX - this.pieces[this.selectedPieceIndex].x;
+      this.offsetY = event.clientY - this.pieces[this.selectedPieceIndex].y;
+
+      document.addEventListener('mousemove', this.handleDrag.bind(this));
+      document.addEventListener('mouseup', this.endDrag.bind(this));
+    }*/
   }
 
-  movePiece(piece: PuzzlePiece) {
-    const piecePosition = this.findPiecePosition(piece);
-    
-    // Verifica si la pieza seleccionada está adyacente a la pieza vacía
-    if (this.isAdjacent(piecePosition)) {
-      // Intercambia las posiciones de la pieza y la pieza vacía
-      [this.puzzle[piecePosition.row][piecePosition.col], this.puzzle[this.emptyPosition.row][this.emptyPosition.col]] =
-        [this.puzzle[this.emptyPosition.row][this.emptyPosition.col], this.puzzle[piecePosition.row][piecePosition.col]];
+  endDrag() {
+    document.removeEventListener('touchmove', this.handleDrag.bind(this));
+    document.removeEventListener('touchend', this.endDrag.bind(this));
 
-      // Actualiza la posición de la pieza vacía
-      this.emptyPosition = piecePosition;
+   
+    this.selectedPieceIndex = -1;
+    this.offsetX = 0;
+    this.offsetY = 0;
+
+    this.checkPuzzle();
+  }
+
+  handleDrag(event: TouchEvent) {
+    event.preventDefault();
+    const touch = event.touches[0];
+    if (this.selectedPieceIndex !== -1) {
+      this.pieces[this.selectedPieceIndex].x = touch.clientX - this.offsetX;
+      this.pieces[this.selectedPieceIndex].y = touch.clientY - this.offsetY ;
+
+      this.checkProximity();
     }
   }
 
-  findPiecePosition(piece: PuzzlePiece): { row: number, col: number } {
-    for (let row = 0; row < this.puzzle.length; row++) {
-      for (let col = 0; col < this.puzzle[row].length; col++) {
-        if (this.puzzle[row][col].id === piece.id) {
-          return { row, col };
-        }
+  checkProximity() {
+    const snapDistance = 20; // Ajusta la distancia de proximidad según sea necesario
+    const containerWidth = window.innerWidth;
+    const containerHeight = window.innerHeight;
+    const piece = this.pieces[this.selectedPieceIndex];
+
+    for (let i = 0; i < this.pieces.length; i++) {
+      
+      const originalPiece = this.originalPositions[i];
+      
+
+      const distance = Math.sqrt(
+        Math.pow(piece.x - originalPiece.x, 2) + Math.pow(piece.y - originalPiece.y, 2)
+      );
+      
+      if (distance < snapDistance) {
+        // La pieza está lo suficientemente cerca, ajustar automáticamente
+        this.pieces[this.selectedPieceIndex].x = originalPiece.x;
+        this.pieces[this.selectedPieceIndex].y = originalPiece.y;
+
+        this.pieces[this.selectedPieceIndex].placed = true;
+        break;
+      }
+    }  
+
+  }
+  
+  checkPuzzle(){
+    let correct = true;
+    for(let i = 0;i< this.pieces.length;i++){
+      const piece = this.pieces[i];
+      const originalPiece = this.originalPositions[i];
+
+      if(piece.x !== originalPiece.x || piece.y !== originalPiece.y){
+        correct = false;
+        break;
       }
     }
-    return { row: -1, col: -1 }; // No debería ocurrir si la lógica es correcta
+
+    if(correct){
+      this.completed = true;
+      console.log('¡Rompecabezas resuelto correctamente!');
+    } else{
+      this.completed = false;
+      console.log('El rompecabezas aún no está resuelto correctamente.');
+    }
   }
+  
+  ngOnInit(){
 
-  isAdjacent(position: { row: number, col: number }): boolean {
-    const rowDiff = Math.abs(position.row - this.emptyPosition.row);
-    const colDiff = Math.abs(position.col - this.emptyPosition.col);
-
-    return (rowDiff === 1 && colDiff === 0) || (rowDiff === 0 && colDiff === 1);
-  }
-
-  ngOnInit() {
   }
 
 }
