ea6bafee7abfca384cde5e0ab440ae498775f88a
HodeiOlivas <96474092+HodeiOlivas@users.noreply.github.com>
2024-02-02 10:17:51 +0100

    Arazoa konponduta

--- argu-app/src/app/app-routing.module.ts
+++ argu-app/src/app/app-routing.module.ts
@@ -1,35 +1,3 @@
-<<<<<<< Updated upstream
-import { NgModule } from '@angular/core';
-import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
-
-const routes: Routes = [
-  {
-    path: 'home',
-    loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)
-  },
-  {
-    path: '',
-    redirectTo: 'home',
-    pathMatch: 'full'
-  },  {
-    path: 'ordenatu-argazkiak',
-    loadChildren: () => import('./ordenatu-argazkiak/ordenatu-argazkiak.module').then( m => m.OrdenatuArgazkiakPageModule)
-  },
-  {
-    path: 'puzzlea',
-    loadChildren: () => import('./puzzlea/puzzlea.module').then( m => m.PuzzleaPageModule)
-  },
-
-];
-
-@NgModule({
-  imports: [
-    RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules })
-  ],
-  exports: [RouterModule]
-})
-export class AppRoutingModule { }
-=======
 import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 import { HizkiSopaComponent } from './hizki-sopa/hizki-sopa.component';
@@ -56,18 +24,8 @@ const routes: Routes = [
     pathMatch: 'full'
   },
   {
-    path: 'kantua',
-    loadChildren: () => import('./kantua/kantua.module').then( m => m.KantuaPageModule)
-  },
-  {
-    path: 'hitzak-lotu',
-    loadChildren: () => import('./hitzak-lotu/hitzak-lotu.module').then( m => m.HitzakLotuPageModule)
-  },
-  {
-    path: 'sentimenduak',
-    loadChildren: () => import('./sentimenduak/sentimenduak.module').then( m => m.SentimenduakPageModule)
-  },
-  {
+
+  
     path: 'intro',
     loadChildren: () => import('./intro/intro.module').then( m => m.IntroPageModule)
   },
@@ -81,4 +39,3 @@ const routes: Routes = [
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
->>>>>>> Stashed changes
