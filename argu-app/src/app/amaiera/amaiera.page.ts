4a6f66be7bb803b4472b8e040dc01802059d146c
AitzolSotoUniEibar <116349384+AitzolSotoUniEibar@users.noreply.github.com>
2024-02-05 12:16:39 +0100

    Amaiera 2.0

--- argu-app/src/app/amaiera/amaiera.page.ts
+++ argu-app/src/app/amaiera/amaiera.page.ts
@@ -7,9 +7,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AmaieraPage implements OnInit {
 
+  audio_1 : any
+
   constructor() { }
 
   ngOnInit() {
+    this.audio_1 = new Audio();
+    this.audio_1.src = 'assets/audio/Amaiera/Amaierako-audioa.mp3';
+    this.audio_1.load();
+    this.audio_1.play();
   }
 
 }
