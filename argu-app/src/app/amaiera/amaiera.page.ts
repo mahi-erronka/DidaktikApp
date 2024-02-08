56efdf7f075f58c3851c3b303f1c7b69ad44f120
AitzolSotoUniEibar <116349384+AitzolSotoUniEibar@users.noreply.github.com>
2024-02-08 11:35:16 +0100

    Login,Register,Puntuazioa

--- argu-app/src/app/amaiera/amaiera.page.ts
+++ argu-app/src/app/amaiera/amaiera.page.ts
@@ -1,3 +1,4 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -8,10 +9,30 @@ import { Component, OnInit } from '@angular/core';
 export class AmaieraPage implements OnInit {
 
   audio_1 : any
+  puntuazioa = '10'
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  partida_gorde(){
+    const apiUrl = 'http://192.168.73.8:80/api/partidak';
+    const requestBody = { id: 1,erabiltzailea_id: 1,puntuazioa: this.puntuazioa};
+
+        this.http.post(apiUrl,requestBody).subscribe(
+          (response) => {
+
+          },
+          (error) => {
+            // Handle error
+            console.error('Error datuak gordetzerakoan', error);
+
+          }
+        );
+  }
 
   ngOnInit() {
+    this.partida_gorde()
+
+
     this.audio_1 = new Audio();
     this.audio_1.src = 'assets/audio/Amaiera/Amaierako-audioa.mp3';
     this.audio_1.load();
